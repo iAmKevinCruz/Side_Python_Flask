109a27e1734d3ea0d5dff22e918cf6fcea82a315
iAmKevinCruz <kcruzroyalty@gmail.com>
2021-09-20 16:37:07 -0400

    added more api functionality

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request
 from mock_data import mock_data
 app = Flask(__name__)
 
@@ -40,9 +40,39 @@ def address():
     return f"{address['number']} {address['street']}"
 
 
-@app.route("/api/catalog")
+@app.route("/api/catalog", methods=["GET"])
 def get_catalog():
+    print(request.headers)
     return json.dumps(mock_data)
+
+
+@app.route("/api/catalog", methods=["POST"])
+def save_product():
+    product = request.get_json()
+
+    if not "price" in product or product["price"] <= 0:
+        abort(400, "Price is required and should be greater than 0")
+
+    if not "title" in product or len(product["title"]) < 5:
+        abort(400, "Title is required and must be at least 5 characters long")
+
+    mock_data.append(product)
+    product["_id"] = len(product["title"])
+    return json.dumps(mock_data)
+
+
+@app.route("/api/catalog/<cat>")
+def get_category(cat):
+    found = False
+    categories = []
+    for prod in mock_data:
+        if prod["category"].lower() == cat.lower():
+            found = True
+            categories.append(prod)
+    if found:
+        return json.dumps(categories)
+    elif not found:
+        abort(404)
 
 
 @app.route("/api/categories")
@@ -54,6 +84,26 @@ def get_categories():
             categories.append(category)
 
     return json.dumps(categories)
+
+
+@app.route("/api/product/<id>")
+def get_id(id):
+    found = False
+    for prod in mock_data:
+        if prod["_id"] == id:
+            found = True
+            return json.dumps(prod)
+    if not found:
+        abort(404)
+
+
+@app.route("/api/product/cheapest")
+def get_cheapest():
+    cheap = mock_data[0]
+    for prod in mock_data:
+        if prod["price"] < cheap["price"]:
+            cheap = prod
+    return json.dumps(cheap)
 
 
 app.run(debug=True)
